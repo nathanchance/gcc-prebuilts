eadf5fab34ef52867bb7b29b8a4458312655af7d
Nathan Chancellor <natechancellor@gmail.com>
2017-09-15 23:17:58 -0700

    aarch64-gnu-linux-android 7.2.1: 20170916
    Compiled on Arch Linux x86_64
    
    Kernel version: 4.13.2 #1 SMP PREEMPT Wed Sep 13 15:13:32 MST 2017
    Host GCC version: 7.2.0
    Make version: 4.2.1
    
    Compiled with crosstool-NG: http://crosstool-ng.github.io/
    GCC source: https://git.linaro.org/toolchain/gcc.git/log/?h=gcc-7-branch
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- aarch64-gnu-linux-android/sysroot/lib/libstdc++.so.6.0.24-gdb.py
+++ aarch64-gnu-linux-android/sysroot/lib/libstdc++.so.6.0.24-gdb.py
@@ -19,8 +19,8 @@ import gdb
 import os
 import os.path
 
-pythondir = '/home/nathan/Toolchains/build/out/aarch64-gnu-linux-android/share/gcc-7.2.1/python'
-libdir = '/home/nathan/Toolchains/build/out/aarch64-gnu-linux-android/aarch64-gnu-linux-android/lib/../lib64'
+pythondir = '/home/nathan/TCs/build/out/aarch64-gnu-linux-android/share/gcc-7.2.1/python'
+libdir = '/home/nathan/TCs/build/out/aarch64-gnu-linux-android/aarch64-gnu-linux-android/lib/../lib64'
 
 # This file might be loaded when there is no current objfile.  This
 # can happen if the user loads it manually.  In this case we don't
